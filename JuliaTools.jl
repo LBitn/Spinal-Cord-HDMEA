3f14887bd1d4da30f11a165683d9546d4494898b
LBitn <lbitnami@gmail.com>
2022-09-07 12:35:17 -0500

    07-sep

--- JuliaTools.jl
+++ JuliaTools.jl
@@ -15,12 +15,18 @@ using DataFrames
 using Statistics
 using Distributions
 using BinningAnalysis
+using Plots
+using HistogramThresholding
+using ImageContrastAdjustment
+using Suppressor
+using StatsBase
+using SignalAnalysis
+using DSP
 
 export div_ab
 export size_segments
 export Get_chunks
 export SavePaths
-export debug_list_vals
 export VariablesBRW
 export searchdir
 export Digital2Analogue
@@ -30,6 +36,21 @@ export Density
 export neighborgs
 export MeanΔxCI
 export Get_Groups
+export FillingHolesCrux
+export FigureGroups
+export NuevePlots
+export Thresholding
+export Zplot
+export ZW
+export Z0
+export ΔV
+export SaturacionNegativaTemporal
+export SegmentsComplet
+export ms2frames
+export FiltroMUAremez
+export SaturacionPositiva
+export TimesPotentialDischarge
+
 
 # -------------------------------------------------------------------------------------------------------- #
 """
@@ -133,16 +154,6 @@ function VariablesBRW( FILEBRW::String )
 
     return Variables, FILEPATHS
 
-end
-# -------------------------------------------------------------------------------------------------------- #
-# using DataFrames
-"""
-debug_list_vals( m::Module = Main )-> vs::Vector{Symbol}
-    Creates a list of the names of the variables presents in the current workspace
-"""
-function debug_list_vals( m::Module = Main )
-    vs = [ name for name in sort!( names( m ) ) if isdefined( m, name ) ];
-    return vs
 end
 # -------------------------------------------------------------------------------------------------------- #
 # using JLD
@@ -318,7 +329,7 @@ end
 # -------------------------------------------------------------------------------------------------------- #
 """
     Get_Groups( W::Vector{Int64} ) -> grupos::Vector{Any}, loose::( Vector{Any} )
-       Groups of adjacent channels are formed from the initial indexes W. Those channels separated from the bulk of the others is considered loose ones.
+       Groups of adjacent channels are formed from the initial indexes W. Those channels separated from the bulk of the others are considered loose ones.
 """
 function Get_Groups( W::Vector{Int64} )
     grupos = [ ];
@@ -404,6 +415,434 @@ function MeanΔxCI( W::Vector, percent::Float64 )
     C1 = xmean + ( Δx * Distributions.quantile( d, percent ) );
     C2 = xmean - ( Δx * Distributions.quantile( d, percent ) );
     return xmean, Δx, C1, C2
+end
+
+# -------------------------------------------------------------------------------------------------------- #
+# using Statistics
+# using BinningAnalysis
+"""
+"""
+function TimesPotentialDischarge( variability::Matrix{Float64} )
+    R = copy( variability ); fill!( R, 0 );
+    WT = zeros( size( variability, 2 ) );
+    for j = 1:size( variability, 2 );
+        W = variability[ :, j ];
+        Todos, t = Thresholding( W );
+        R[ :, j ] = sum( Todos, dims = 2 );
+        xmean, xerror = jackknife( identity, R[ :, j ] );
+        W = R[ :, j ];
+        aux = union( findall( W .> xmean + std( W ) + xerror ), findall( W .< xmean - std( W ) - xerror ) );
+        WT[ j ] = length( aux );
+    end
+
+    xmean, xerror = jackknife( identity, WT );
+    Test04 = union( findall( WT .> xmean + 2*std( WT ) ), findall( WT .< xmean - 2*std( WT )  ) );
+    return Test04
+end
+
+# -------------------------------------------------------------------------------------------------------- #
+"""
+"""
+function SaturacionPositiva( Variables::Dict{String, Any}, data )
+
+    nChs = length( Variables[ "Layout" ] );
+    SamplingRate = Variables[ "SamplingRate" ];
+    MinVolt = Float64( Variables[ "MinVolt" ] + 1 );
+    channels = collect( 1:nChs );
+
+    MinVoltSat = sum( Int.( data .== MinVolt ), dims = 2 );
+
+    if length( findall( vec( MinVoltSat ) .== 0 ) ) == ( nChs - 1 )
+        earth = setdiff( channels, findall( vec( MinVoltSat ) .== 0 ) )[ ];
+        MinVoltSatChannels = [ ];
+        data[ earth, : ] .= 0;
+    else
+        SatFramesLimit = ceil( Int, SamplingRate*0.005 );
+        NineNinePercent = ceil( Int, binsize*0.99 );
+        MinVoltSatChannels = findall( vec( NineNinePercent .>= MinVoltSat .>= SatFramesLimit ) );
+        aux = findall( vec( MinVoltSat ) .>= NineNinePercent );
+        if length( aux ) == 1
+            earth = aux[ 1 ];
+            data[ earth, : ] .= 0;
+        else
+            error( "there is something fishy here" );
+        end
+    end;
+    return data, earth, MinVoltSatChannels
+end
+
+# -------------------------------------------------------------------------------------------------------- #
+# using DSP
+"""
+"""
+function FiltroMUAremez( Variables::Dict{String, Any}, canal::Vector{Float64} )
+
+    SamplingRate = Variables[ "SamplingRate" ];
+    lF = 300; fac = 10; HF = 3000;
+    NYQ = floor( Int, SamplingRate / 2 );
+    order = Int( floor( ( SamplingRate / lF ) / 5 ) );
+
+    bpass = remez(
+        ( order + 1 ), [ ( 0, lF - fac ) => 0, ( lF, HF ) => 1, ( HF + fac, NYQ ) => 0 ],
+            Hz = SamplingRate );
+    MUA = filtfilt( DSP.Filters.PolynomialRatio( bpass, [ 1.0 ] ), canal );
+    return MUA
+end
+
+# -------------------------------------------------------------------------------------------------------- #
+"""
+"""
+function ms2frames( time::Real, SamplingRate::Real )
+    if time != 0
+        x = ceil( Int, ( time * SamplingRate ) / 1000 );
+    else
+        x = 1
+    end
+    return x
+end
+
+# -------------------------------------------------------------------------------------------------------- #
+# using SignalAnalysis
+"""
+"""
+function SegmentsComplet( Temp02, antes, SegmentDuration, SamplingRate, limit, MUA )
+    WindowIni = Temp02 .- ms2frames( antes, SamplingRate );
+    total = antes*2 + SegmentDuration;
+    WindowEnd = WindowIni .+ ms2frames( total, SamplingRate );
+    SegmentDuration = ms2frames( SegmentDuration, SamplingRate );
+    SegmentsCompletos = [ ];
+    for aux = 1:length( Temp02 )
+        wi = WindowIni[ aux ]; we = WindowEnd[ aux ];
+        results = [ ]; wis = [ ];
+        while ( we <= limit ) && wi >= 1 && ( wi + SegmentDuration < we)
+            wbin = MUA[ wi:wi + SegmentDuration ];
+            push!( results, energy( wbin ) );
+            push!( wis, wi );
+            wi = wi + 1;
+        end
+        if !isempty( results )
+            wfinal = wis[ results .== maximum( results ) ][ 1 ];
+            wfinal = [ wfinal, wfinal + SegmentDuration ];
+            push!( SegmentsCompletos, wfinal )
+        end
+    end
+    return Matrix( hcat( SegmentsCompletos... )' );
+end
+
+# -------------------------------------------------------------------------------------------------------- #
+"""
+"""
+function SaturacionNegativaTemporal( Variables::Dict{String, Any}, data::VecOrMat{Float64} )
+
+    SamplingRate = Variables[ "SamplingRate" ];
+    MaxVolt = Float64( Variables[ "MaxVolt" ] - 1 );
+    binsize = size( data, 2 );
+
+    MaxVoltSatChannels = findall( vec( sum( Int.( data .== MaxVolt ), dims = 2 ) .!= 0 ) );
+    MaxVoltFrames = [ ];
+    for k = 1:length( MaxVoltSatChannels )
+        push!( MaxVoltFrames, findall( data[ MaxVoltSatChannels[ k ], : ] .== MaxVolt ) );
+    end
+
+    maximo_permitido = ceil( Int, SamplingRate*0.3 );
+    DiscartedChannels = MaxVoltSatChannels[ length.( MaxVoltFrames) .> maximo_permitido ];
+    MaxSatReparables = setdiff( MaxVoltSatChannels, DiscartedChannels );
+    dataTemp = copy( data );
+
+    for k = 1:length( MaxSatReparables );
+
+        Channel4Repair = MaxSatReparables[ k ];
+        Frames4Repair = MaxVoltFrames[ MaxVoltSatChannels .==  Channel4Repair ][ ];
+        FrameIndex = collect( 1:binsize );
+        FramePool = setdiff( FrameIndex, Frames4Repair );
+        FrameSample = sample( FramePool, length( Frames4Repair ) );
+        VoltageSample = data[ Channel4Repair, FrameSample ];
+        dataTemp[ Channel4Repair, Frames4Repair ] .= VoltageSample;
+
+    end
+    return dataTemp, DiscartedChannels
+
+end
+
+# -------------------------------------------------------------------------------------------------------- #
+"""
+"""
+function ΔV( Variables::Dict{String, Any}, BIN::Matrix{Float64}, ΔT::Int64, descartados::Vector{Int64} )
+
+    SamplingRate = Variables[ "SamplingRate" ][ 1 ];
+    ΔT = ms2frames( ΔT, SamplingRate );
+
+    STD = vec( std( ( BIN - circshift( BIN, ( 0, ΔT )  ) ), dims = 2 ) );
+    STD2 = copy( STD ); STD2[ descartados ] .= 0;
+
+    return STD, STD2
+
+end
+
+# -------------------------------------------------------------------------------------------------------- #
+"""
+"""
+function Z0( X )
+
+    Z = zeros( Int, 4096 );
+    Z[ X ] = Z[ X ] .+ 1;
+    Z = reverse( reshape( Z, 64, 64 )', dims = 1 );
+
+    return Z
+end
+
+# -------------------------------------------------------------------------------------------------------- #
+"""
+"""
+function ZW( X )
+    Z = reverse( reshape( X, 64, 64 )', dims = 1 );
+    return Z
+end
+
+# -------------------------------------------------------------------------------------------------------- #
+"""
+"""
+function Zplot( Z, which, cm = :greys )
+    if which == "0"
+        Z = Z0( Z );
+    elseif which == "W"
+        Z = ZW( Z );
+    end
+    F = heatmap(
+            Z,
+            aspect_ratio = 1,
+            c = cm,
+            axis = ( [ ], false ),
+            wsize = ( 400, 400 ),
+            #cbar = :none
+        );
+    return F
+
+end
+
+# -------------------------------------------------------------------------------------------------------- #
+# using HistogramThresholding
+# using ImageContrastAdjustment
+# using Suppressor
+# using StatsBase
+function Thresholding( W::VecOrMat{Float64} )
+    W = copy( vec( W ) ); n = length( W );
+    edges, conteo = HistogramThresholding.build_histogram( W, length( keys( countmap( W ) ) ) );
+
+    t = zeros( 9 ); Todos = zeros( n, length( t ) );
+
+    @suppress begin
+
+        thr = find_threshold( UnimodalRosin( ), conteo[ 1:end ], edges );
+        Todos[ W .>= thr, 1 ] .= 1;
+        t[ 1 ] = thr;
+        thr = find_threshold( MinimumIntermodes( ), conteo[ 1:end ], edges );
+        Todos[ W .>= thr, 2 ] .= 1;
+        t[ 2 ] = thr;
+        thr = find_threshold( Intermodes( ), conteo[ 1:end ], edges );
+        Todos[ W .>= thr, 3 ] .= 1;
+        t[ 3 ] = thr;
+        thr = find_threshold( MinimumError( ), conteo[ 1:end ], edges );
+        Todos[ W .>= thr, 4 ] .= 1;
+        t[ 4 ] = thr;
+        thr = find_threshold( Moments( ), conteo[ 1:end ], edges );
+        Todos[ W .>= thr, 5 ] .= 1;
+        t[ 5 ] = thr;
+        thr = find_threshold( Otsu( ), conteo[ 1:end ], edges );
+        Todos[ W .>= thr, 6 ] .= 1;
+        t[ 6 ] = thr;
+        thr = find_threshold( Entropy( ), conteo[ 1:end ], edges );
+        Todos[ W .>= thr, 7 ] .= 1;
+        t[ 7 ] = thr;
+        thr = find_threshold( Balanced( ), conteo[ 1:end ], edges );
+        Todos[ W .>= thr, 8 ] .= 1;
+        t[ 8 ] = thr;
+        thr = find_threshold( Yen( ), conteo[ 1:end ], edges );
+        Todos[ W .>= thr, 9 ] .= 1;
+        t[ 9 ] = thr;
+    end
+
+    return Int.( Todos ), t
+
+end
+
+# -------------------------------------------------------------------------------------------------------- #
+# using Plots
+function NuevePlots( Todos::Matrix{Int64} )
+
+    AbstractImageBinarizationAlgorithm = [
+        "UnimodalRosin",
+        "MinimumIntermodes",
+        "Intermodes",
+        "MinimumError",
+        "Moments",
+        "Otsu",
+        "Entropy",
+        "Balanced",
+        "Yen"
+    ];
+
+    m = 1
+    xmessage = string( AbstractImageBinarizationAlgorithm[ m ], ": ", length( findall( Todos[ :, m ] .== 1 ) )," channels" );
+    T1 = Zplot( Todos[ :, m ], "W" );
+    T1 = plot!( title = xmessage, cbar = :none );
+
+    m = 2
+    xmessage = string( AbstractImageBinarizationAlgorithm[ m ], ": ", length( findall( Todos[ :, m ] .== 1 ) )," channels" );
+    T2 = Zplot( Todos[ :, m ], "W" );
+    T2 = plot!( title = xmessage, cbar = :none );
+
+    m = 3
+    xmessage = string( AbstractImageBinarizationAlgorithm[ m ], ": ", length( findall( Todos[ :, m ] .== 1 ) )," channels" );
+    T3 = Zplot( Todos[ :, m ], "W" );
+    T3 = plot!( title = xmessage, cbar = :none );
+
+    m = 4
+    xmessage = string( AbstractImageBinarizationAlgorithm[ m ], ": ", length( findall( Todos[ :, m ] .== 1 ) )," channels" );
+    T4 = Zplot( Todos[ :, m ], "W" );
+    T4 = plot!( title = xmessage, cbar = :none );
+
+    m = 5
+    xmessage = string( AbstractImageBinarizationAlgorithm[ m ], ": ", length( findall( Todos[ :, m ] .== 1 ) )," channels" );
+    T5 = Zplot( Todos[ :, m ], "W" );
+    T5 = plot!( title = xmessage, cbar = :none );
+
+    m = 6
+    xmessage = string( AbstractImageBinarizationAlgorithm[ m ], ": ", length( findall( Todos[ :, m ] .== 1 ) )," channels" );
+    T6 = Zplot( Todos[ :, m ], "W" );
+    T6 = plot!( title = xmessage, cbar = :none );
+
+    m = 7
+    xmessage = string( AbstractImageBinarizationAlgorithm[ m ], ": ", length( findall( Todos[ :, m ] .== 1 ) )," channels" );
+    T7 = Zplot( Todos[ :, m ], "W" );
+    T7 = plot!( title = xmessage, cbar = :none );
+
+    m = 8
+    xmessage = string( AbstractImageBinarizationAlgorithm[ m ], ": ", length( findall( Todos[ :, m ] .== 1 ) )," channels" );
+    T8 = Zplot( Todos[ :, m ], "W" );
+    T8 = plot!( title = xmessage, cbar = :none );
+
+    m = 9
+    xmessage = string( AbstractImageBinarizationAlgorithm[ m ], ": ", length( findall( Todos[ :, m ] .== 1 ) )," channels" );
+    T9 = Zplot( Todos[ :, m ], "W" );
+    T9 = plot!( title = xmessage, cbar = :none );
+
+    TF = plot( T1, T2, T3, T4, T5, T6, T7, T8, T9, layout = ( 3, 3 ), wsize = ( 700, 700 ), titlefont = ( 8, "arial" ) );
+    return TF
+end
+
+# -------------------------------------------------------------------------------------------------------- #
+function FigureGroups( grupos::Vector, loose::Vector = [ ], cm = :twilight )
+    Z = zeros( Int, 4096 );
+
+    for i = 1:size( grupos, 1 )
+        Z[ grupos[ i ] ] .= floor( Int, log( length( grupos[ i ] ) ) ) + 2 ;
+    end
+    Z[ Int.( loose ) ] .= 1
+    Z = reverse( reshape( Z, 64, 64 )', dims = 1 )
+    F = heatmap(
+        Z,
+        aspect_ratio = 1,
+        c = cm,
+        axis = ( [ ], false ),
+        wsize = ( 400, 400 ),
+        cbar = :none );
+    return F
+end
+
+# -------------------------------------------------------------------------------------------------------- #
+# using JuliaTools.neighborgs
+function FillingHolesCrux( SatChannelsVar::Vector{Int64} )
+
+    Z = reverse( reshape( 1:4096, 64, 64 )', dims = 1 );
+    arriba = Z[ 64, 2:( end - 1 ) ]; abajo = Z[ 1, 2:( end - 1 ) ];
+    izquierda = Z[ 2:( end - 1 ), 1 ]; derecha = Z[ 2:( end - 1 ), 64 ];
+    esquinas = vcat( Z[ 1, 1 ], Z[ 64, 64 ], Z[ 64, 1 ], Z[ 1, 64 ] );
+    bordes = vcat( arriba, abajo, izquierda, derecha, esquinas );
+    bordes_noesquinas = vcat( arriba, abajo, izquierda, derecha );
+
+    P = true
+    while P
+        X1 = setdiff( setdiff( setdiff( 1:4096, SatChannelsVar ) ), bordes );
+        p1 = [ ];
+        for i = 1:length( X1 )
+
+            x = X1[ i ];
+            A, _ = neighborgs( x, 1 );
+            CruzH = [ A[ 2, 1 ], A[ 2, 3 ] ];
+            CruzV = [ A[ 1, 2 ], A[ 3, 2 ] ];
+
+            x1 = length( CruzH[ CruzH .∈ [ SatChannelsVar ] ] ) == 2;
+            x2 = length( CruzV[ CruzV .∈ [ SatChannelsVar ] ] ) == 2;
+
+            if ( x1 || x2 )
+                push!( p1, x )
+            end
+        end
+
+        X1 = intersect( arriba, setdiff( setdiff( 1:4096, SatChannelsVar ) ) );
+
+        p2 = [ ];
+        for i = 1:length( X1 )
+
+            x = X1[ i ];
+            A, _ = neighborgs( x, 1 );
+                Cruz = [ A[ 2, 1 ], A[ 2, 3 ] ];
+                x1 = length( Cruz[ Cruz .∈ [ SatChannelsVar ] ] ) == 2;
+            if x1
+                push!( p2, x )
+            end
+        end
+
+        X1 = intersect( abajo, setdiff( setdiff( 1:4096, SatChannelsVar ) ) );
+
+        p3 = [ ];
+        for i = 1:length( X1 )
+
+            x = X1[ i ];
+            A, _ = neighborgs( x, 1 );
+                Cruz = [ A[ 1, 1 ], A[ 1, 3 ] ];
+                x1 = length( Cruz[ Cruz .∈ [ SatChannelsVar ] ] ) == 2;
+            if x1
+                push!( p3, x )
+            end
+        end
+
+        X1 = intersect( vcat( izquierda, derecha ), setdiff( setdiff( 1:4096, SatChannelsVar ) ) );
+
+        p4 = [ ];
+        for i = 1:length( X1 )
+
+            x = X1[ i ];
+            A, _ = neighborgs( x, 1 );
+                Cruz = [ A[ 1, 2 ], A[ 3, 2 ] ];
+                x1 = length( Cruz[ Cruz .∈ [ SatChannelsVar ] ] ) == 2;
+            if x1
+                push!( p4, x )
+            end
+        end
+
+         X1 = intersect( esquinas, setdiff( setdiff( 1:4096, SatChannelsVar ) ) );
+         p5 = [ ];
+         for i = 1:length( X1 )
+             x = X1[ i ];
+             _, v = neighborgs( x, 1 );
+
+             if length( v[ v .∈ [ SatChannelsVar ] ] ) >= 2
+                 push!( p5, x )
+             end
+         end
+        posibles = [ ];
+        posibles = vcat( Int.( p1 ), Int.( p2 ), Int.( p3 ), Int.( p4 ), Int.( p5 ) );
+
+        if isempty( posibles )
+            P = false
+        else
+            SatChannelsVar = vcat( posibles, SatChannelsVar );
+        end
+
+    end
+    return Int.( SatChannelsVar )
 end
 
 end
